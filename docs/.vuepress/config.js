9432aacb8ea87fe74e67c0890bba7689950cbcba
debmus <contact@feranet.fr>
2020-11-15 13:13:01 +0100

    remove: app link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,7 +20,6 @@ module.exports = {
         searchPlaceholder: 'Rechercher ...',
         nav: [
             { text: 'Wiki', link: '/' },
-            { text: 'Outils', link: 'https://app.feranet.fr' },
             { text: 'Images', link: 'https://cloud.feranet.fr/s/TWYWF6KrpdfXCf9' },
             { text: 'GitHub', link: 'https://github.com/debmus/feranet.fr' }
         ],
